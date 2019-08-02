8226e344c8332140a474e34316221776e9ca2e97
lkmklsmn <lkmklsmn@gmail.com>
2019-08-02 16:29:40 -0500

    add download link

--- download.sh
+++ download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # downloads XXXGb dataset files
-wget https://hmgubox.helmholtz-muenchen.de/XXXXXXX -O 2019_Strunz.tar
+wget https://hmgubox.helmholtz-muenchen.de/f/492f4319237a464f9a28/ -O 2019_Strunz.tar
 mkdir data; cd data
 tar -xvf ../2019_Strunz.tar
