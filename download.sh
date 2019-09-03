363d60e36999098b6f6ec6f9ed170f98e7d89746
lkmklsmn <lkmklsmn@gmail.com>
2019-09-03 14:29:25 -0500

    fix URL

--- download.sh
+++ download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # downloads 9Gb dataset files
-wget https://hmgubox.helmholtz-muenchen.de/f/492f4319237a464f9a28/ -O 2019_Strunz.tar
+wget https://hmgubox.helmholtz-muenchen.de/f/492f4319237a464f9a28/?dl=1 -O 2019_Strunz.tar
 mkdir data; cd data
 tar -xvf ../2019_Strunz.tar
