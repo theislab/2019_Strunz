143981fa26bf23cd8f96342f2b251656652dbdf2
lkmklsmn <lkmklsmn@gmail.com>
2023-11-20 10:43:00 -0600

    Update download link

--- download.sh
+++ download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # downloads 9Gb dataset files
-wget https://hmgubox.helmholtz-muenchen.de/f/492f4319237a464f9a28/?dl=1 -O 2019_Strunz.tar
+wget https://1drv.ms/f/s!AiwuS6Fz15R0gsgiXtjxJZqr-BDt0A?e=nCnnJ5 -O 2019_Strunz.tar
 mkdir data; cd data
 tar -xvf ../2019_Strunz.tar
